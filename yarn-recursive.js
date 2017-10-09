92e978f82f2e8a15e940fe31dc60a9ee311efc4c
Nicolas Rigaudiere <nicolas@blueboard.io>
2017-10-09 15:03:58 +0200

    Add yarn.lock and exec script

---
+++ yarn-recursive.js
@@ -0,0 +1,44 @@
+#!/usr/bin/env node
+
+const path = require('path');
+const shell = require('shelljs');
+const argv = require('yargs').argv;
+
+function packageJsonLocations(dirname) {
+  return shell.find(dirname)
+    .filter(fname => !(fname.indexOf('node_modules') > -1 || fname[0] === '.') && path.basename(fname) === 'package.json')
+    .map(fname => path.dirname(fname));
+}
+
+function yarn(directoryName) {
+  let result = shell.exec('yarn');
+  shell.cd(directoryName);
+  
+  console.log('Current yarn path: ' + directoryName + '/package.json...');
+
+  return {
+    directoryName: directoryName,
+    exitCode: result.code
+  };
+}
+
+function filterRoot(directoryName) {
+  console.log('Root filtering');
+
+  return path.normalize(directoryName) === path.normalize(process.cwd());
+}
+
+if (require.main === module) {
+  let exitCode = packageJsonLocations(process.cwd())
+    .filter(argv.skipRoot ? filterRoot : filtered => filtered)
+    .map(yarn)
+    .reduce((code, result) =>result.exitCode > code ? result.exitCode : code, 0);
+
+  process.exit(exitCode);
+}
+
+module.exports = {
+  packageJsonLocations: packageJsonLocations,
+  yarn: yarn,
+  filterRoot: filterRoot
+};
